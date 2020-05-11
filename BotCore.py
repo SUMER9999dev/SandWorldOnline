d6c4398adb9b16cfd9c688ddb7c22e20e51b52b3
SUMER9999dev <59053498+SUMER9999dev@users.noreply.github.com>
2020-05-11 14:42:07 +0300

    Update BotCore.py

--- BotCore.py
+++ BotCore.py
@@ -7,7 +7,7 @@ from discord.ext.commands import has_permissions, MissingPermissions, MissingReq
 import os
 client = commands.Bot(command_prefix = "sw!")
 client.remove_command("help")
-BotCoreVer = "1.4.5"
+BotCoreVer = "1.1.3"
 @client.event
 async def on_ready():
     await client.change_presence(status=discord.Status.do_not_disturb, activity=discord.Game(name="sw!help"))
@@ -46,7 +46,6 @@ async def shopcmd(ctx):
         elif shovel == 4:
             em = discord.Embed(title="SandWorld Online Alpha", type="rich", description=":warning: You need sw!RegProfile before do that!", colour=0xffdd00)
             await msg.edit(embed=em)
-        await msg.clear_reactions()
     elif rea.emoji == "🗡️":
         TheSword = SandWorldCore.CreateItem(0, 7, "Rusty sword")
         Sword = SandWorldCore.BuyItem(ctx.author.id, TheSword, 100)
@@ -62,7 +61,6 @@ async def shopcmd(ctx):
         elif Sword == 4:
             em = discord.Embed(title="SandWorld Online Alpha", type="rich", description=":warning: You need sw!RegProfile before do that!", colour=0xffdd00)
             await msg.edit(embed=em)
-        await msg.clear_reactions()
     elif rea.emoji == "🦾":
         TheArmor = SandWorldCore.CreateArmor(5, "Old armor")
         Armor = SandWorldCore.BuyArmor(ctx.author.id, TheArmor, 120)
@@ -78,7 +76,6 @@ async def shopcmd(ctx):
         elif Armor == 4:
             em = discord.Embed(title="SandWorld Online Alpha", type="rich", description=":warning: You need sw!RegProfile before do that!", colour=0xffdd00)
             await msg.edit(embed=em)
-        await msg.clear_reactions()
     else:
         pass
 @client.command(name="info", description = "info about game.")
